f07880151664da463edb0ccc5e9c121d24b9dbf9
ShadowWolf308 <levyvandervalk@gmail.com>
2022-05-19 10:59:28 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+
+<head>
+    <!--meta data-->
+    <!--usable characters for the website-->
+    <meta charset="utf-8">
+
+    <!--language of the page-->
+    <meta http-equiv="language" content="NL">
+
+    <!--display settings-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--website description-->
+    <meta name="description" content="">
+
+    <!--author data-->
+    <meta name="author" content="Levy van der Valk">
+
+    <!--search words for google-->
+    <meta name="keywords" content="">
+
+    <!--website title in tab-->
+    <title></title>
+
+    <!--linking a .css page-->
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+
+<body>
+    <!--body data-->
+    <header>
+        <!--header data-->
+
+    </header>
+    <form type="post">
+        <input type="text" required placeholder="vul hier je naam in">
+        <textarea id="bericht" required placeholder="vul hier je bericht in"></textarea>
+        <input type="submit">
+    </form>
+    <footer>
+        <!--footer data-->
+
+    </footer>
+    <!--linking a .js file-->
+    <script src="" type="text/javascript"></script>
+</body>
+
+</html>
