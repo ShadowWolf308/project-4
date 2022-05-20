da9ec1a3253e35c09f4b42bcf6acae1f943135e9
ShadowWolf308 <levyvandervalk@gmail.com>
2022-05-20 15:00:51 +0200

    contact af

--- contact.php
+++ contact.php
@@ -1,5 +1,22 @@
 <?php
-    require("dbconnect.php");
+    $error = "";
+    if(isset($_POST['submit'])) {
+        if(!empty($_POST['naam'] && !empty($_POST['bericht']))) {
+            require("dbconnect.php");
+            session_start();
+            if ((!isset($_SESSION['ingelogd'])) || ($_SESSION['ingelogd'] != true)) {
+                $sql = "INSERT INTO contact (gebruiker_id,naam,bericht) VALUES ('0','".trim($_POST['naam'])."','".trim($_POST['bericht'])."')";
+                if ($conn->query($sql) === false) {
+                    die("Error: " . $sql . "<br>" . $conn->error);
+                }
+            } else {
+                $sql = "INSERT INTO contact (gebruiker_id,naam,bericht) VALUES ('".$_SESSION['id']."','".trim($_POST['naam'])."','".trim($_POST['bericht'])."')";
+                if ($conn->query($sql) === false) {
+                    die("Error: " . $sql . "<br>" . $conn->error);
+                }
+            }
+        }
+    }
 ?>
 <!doctype html>
 <html>
@@ -47,10 +64,10 @@
         </nav>
         <div></div>
     </header>
-    <form type="post">
-        <input type="text" required placeholder="vul hier je naam in">
-        <textarea id="bericht" required placeholder="vul hier je bericht in"></textarea>
-        <input type="submit">
+    <form method="POST">
+        <input type="text" id="name" name="naam" required placeholder="vul hier je naam in">
+        <textarea id="bericht" name="bericht" required placeholder="vul hier je bericht in"></textarea>
+        <input type="submit" name="submit" value="versturen">
     </form>
     <footer>
         <!--footer data-->
