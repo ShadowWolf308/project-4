c2e25471f064c4f763a9b854e64d2a7681c4d619
Dokka2 <94675171+Ryan674@users.noreply.github.com>
2022-05-30 11:45:08 +0200

    ingelogd php

---
+++ ingelogd.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+// if ($_SESSION['ingelogd'] != true) {
+//     header("location: inloggen.php");
+// }
+
+?>
+
+<html>
+
+<head>
+    <!--meta data-->
+    <!--usable characters for the website-->
+    <meta charset="utf-8">
+
+    <!--language of the page-->
+    <meta http-equiv="language" content="NL">
+
+    <!--display settings-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--website description-->
+    <meta name="description" content="Website TIGER">
+
+    <!--author data-->
+    <meta name="author" content="Tom_Diede_Levy_Ryan">
+
+    <!--search words for google-->
+    <meta name="keywords" content="">
+
+    <!--website title in tab-->
+    <title>www.TIGER.nl</title>
+
+    <!--linking a .css page-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <!--body data-->
+    <header>
+        <!--header data-->
+        <div></div>
+        <img src="images/logo.png" alt="Logo">
+        <nav>
+            <a href="./index.php">Home</a>
+            <a href="">Product Info</a>
+            <a href="">Kalender</a>
+            <a href="">Artiesten</a>   
+            <a href="">Aanbiedingen</a>
+            <a href="./contact.php">Contact</a>
+            <a href="">Registreren</a>
+        </nav>
+        <div></div>
+    </header>
+    <article class= "text1">
+    
+    </article>
+    
+
+    <footer>
+        <!--footer data-->
+
+    </footer>
+    <!--linking a .js file-->
+    <script src="" type="text/javascript"></script>
+</body>
+
+</html>
