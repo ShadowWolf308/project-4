e97aafdecb8ca2a19ccbd433094fa6eace1c987b
DiedeB <74538290+DiedeB@users.noreply.github.com>
2022-05-31 08:47:12 +0200

    aanbiedingen css

---
+++ Aanbiedingen.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+
+<head>
+    <!--meta data-->
+    <!--usable characters for the website-->
+    <meta charset="utf-8">
+
+    <!--language of the page-->
+    <meta http-equiv="language" content="NL">
+
+    <!--display settings-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--website description-->
+    <meta name="description" content="Website TIGER">
+
+    <!--author data-->
+    <meta name="author" content="Tom_Diede_Levy_Ryan">
+
+    <!--search words for google-->
+    <meta name="keywords" content="">
+
+    <!--website title in tab-->
+    <title>www.TIGER.nl</title>
+
+    <!--linking a .css page-->
+    <link rel="stylesheet" type="text/css" href="css/Aanbiedingen.css">
+</head>
+
+<body>
+    <!--body data-->
+    <header>
+        <!--header data-->
+        <div></div>
+        <img src="images/logo.png" alt="Logo">
+        <nav>
+            <a href="./index.php">Home</a>
+            <a href="">Product Info</a>
+            <a href="">Kalender</a>
+            <a href="">Artiesten</a>   
+            <a href="./Aanbiedingen.php">Aanbiedingen</a>
+            <a href="./contact.php">Contact</a>
+            <a href="./registreren.php">Registreren</a>
+        </nav>
+        <div></div>
+    </header>
+    
+
+    <footer>
+        <!--footer data-->
+
+    </footer>
+    <!--linking a .js file-->
+    <script src="" type="text/javascript"></script>
+</body>
+
+</html>
