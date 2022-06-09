5e69ea4b837531c128f7e8a90be8f45172f10443
tomgroenheide <6019244@mborijnland.nl>
2022-06-09 12:16:18 +0200

    yeaahhhh

--- index.php
+++ index.php
@@ -65,9 +65,21 @@ if(isset($_SESSION['ingelogd']) && $_SESSION['ingelogd'] == true) {
     <article class= "text2">
     <h1>Welkom!</h1><br>
     <p>Welkom op de homepagina van Tiger Energy, ons bedrijf is begonnen met het maken van energy in 2010 toen de wereld toe was aan een nieuwe unieke energydrank beleving.<br>
-
     </p>
-    </article>
+    </article>  
+
+
+<button data-modal-target="#modal">Open Modal</button>
+  <div class="modal" id="modal">
+    <div class="modal-header">
+      <div class="title">Example Modal</div>
+      <button data-close-button class="close-button">&times;</button>
+    </div>
+    <div class="modal-body">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod alias ut illo doloremque eum ipsum obcaecati distinctio debitis reiciendis quae quia soluta totam doloribus quos nesciunt necessitatibus, consectetur quisquam accusamus ex, dolorum, dicta vel? Nostrum voluptatem totam, molestiae rem at ad autem dolor ex aperiam. Amet assumenda eos architecto, dolor placeat deserunt voluptatibus tenetur sint officiis perferendis atque! Voluptatem maxime eius eum dolorem dolor exercitationem quis iusto totam! Repudiandae nobis nesciunt sequi iure! Eligendi, eius libero. Ex, repellat sapiente!
+    </div>
+  </div>
+  <div id="overlay"></div>
 
 
     <footer>
@@ -75,7 +87,7 @@ if(isset($_SESSION['ingelogd']) && $_SESSION['ingelogd'] == true) {
 
     </footer>
     <!--linking a .js file-->
-    <script src="" type="text/javascript"></script>
+    <script src="./js/index.js" type="text/javascript"></script>
 </body>
 
 </html>
